2b54be0778ff16919f36b8470fa085efe48cce9f
Miguel Valdez <eduardovaldeztellez@gmail.com>
2020-05-25 18:46:21 -0500

    primer commit

---
+++ app/src/main/java/com/example/covid/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.covid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button login, create;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login = (Button) findViewById(R.id.btnLogin);
+        create = (Button) findViewById(R.id.btnCreate);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(),crearcuenta.class);
+                startActivityForResult(intent,0);
+            }
+        });
+    }
+}
